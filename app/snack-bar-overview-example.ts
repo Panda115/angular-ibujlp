ddc6fb411363a2dc8f09e94951c3a1a006721797
Panda115 <panda.dev1115@gmail.com>
2022-12-15 02:40:16 +0800

    Initial commit

---
+++ app/snack-bar-overview-example.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+
+/**
+ * @title Basic snack-bar
+ */
+@Component({
+  selector: 'snack-bar-overview-example',
+  templateUrl: 'snack-bar-overview-example.html',
+  styleUrls: ['snack-bar-overview-example.css'],
+})
+export class SnackBarOverviewExample {
+  constructor(public snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
